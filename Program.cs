fab72578e38f6f10450468532558c8a9c9b6c096
Raziel <anaraziel9@gmail.com>
2022-07-27 17:20:13 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escribe tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola, " + nombre);
+
+Console.WriteLine("Escribe la hora");
+string hora = Console.ReadLine();
+Console.WriteLine("La hora es, " + hora);
